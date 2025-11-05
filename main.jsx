fcead3d225b3d6444c955896fb5901d1412d2986
texassoldem <seth@texassoldem.com>
2025-11-05 00:22:21 -0600

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import RookieEconomicPlanner from "./RookieEconomicPlanner";
+import RookieEconomicPlanner from "./RookieEconomicPlanner.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
